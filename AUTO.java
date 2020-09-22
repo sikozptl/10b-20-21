0526a1ea092fb4bf38cb3f3655802e249abf33d8
Ulrich Schneider <mail@ulrichschneider.de>
2020-09-22 11:21:08 +0200

    Eingabeparameter

--- AUTO.java
+++ AUTO.java
@@ -5,6 +5,7 @@ public class AUTO{
     int Leistung;
     int Geschwindigkeit;
     boolean MotorAn;
+    int Hoechstgeschwindigkeit;
 
     //Konstruktor
     public AUTO(){
@@ -13,6 +14,7 @@ public class AUTO{
         Leistung = 100;
         Geschwindigkeit = 0;
         MotorAn = false; //Motor läuft nicht
+        Hoechstgeschwindigkeit = 60;
     }
 
     //Methoden
@@ -37,7 +39,7 @@ public class AUTO{
         //Prüfen, ob Motor läuft (Vergleich mit ==)
         if(MotorAn == true){
             //Das Auto soll nie schneller als 80 fahren!
-            if(Geschwindigkeit < 80){
+            if(Geschwindigkeit < Hoechstgeschwindigkeit){
                 //neue Geschw = alte Geschw +10
                 // = heisst Zuweisung
                 Geschwindigkeit = Geschwindigkeit + 10;
@@ -48,6 +50,43 @@ public class AUTO{
             System.out.println("Motor läuft nicht!");
         }//Prüfung MotorAn Ende
     }
+    
+    //Auto soll nur beschleunigen und bremsen, wenn Motor läuft
+    public void beschleunigen(int kmh){
+        //Prüfen, ob Motor läuft (Vergleich mit ==)
+        if(MotorAn == true){
+            //Das Auto soll nie schneller als 80 fahren!
+            if(Geschwindigkeit < Hoechstgeschwindigkeit){
+                //neue Geschw = alte Geschw +10
+                // = heisst Zuweisung
+                Geschwindigkeit = Geschwindigkeit + 10;
+            }else{
+                System.out.println("Höchstgeschwindigkeit erreicht");
+            }//Prüfung Geschwindigkeit Ende
+        }else{
+            System.out.println("Motor läuft nicht!");
+        }//Prüfung MotorAn Ende
+    }    
+    
+    public void setFarbe(String NeueFarbe){
+        Farbe = NeueFarbe;
+    }
+    
+    //Methode mit Eingabeparameter
+    public void setHersteller(String NeuerHersteller){
+        Hersteller = NeuerHersteller;
+    }
+    
+    //Methode mit Eingabeparameterliste
+    //die Methode soll alles Ändern, was sinnvollerweise geändert werden kann
+    public void setAlles(String NeueFarbe, String NeuerHersteller,
+            int NeueLeistung, int NeueHoechstgeschwindigkeit){
+        Hersteller = NeuerHersteller;
+        Farbe = NeueFarbe;
+        Leistung = NeueLeistung;
+        Hoechstgeschwindigkeit = NeueHoechstgeschwindigkeit;
+    }
+    
     
     public void bremsen(){
         // Geschwindigkeit soll >= 0 bleiben
